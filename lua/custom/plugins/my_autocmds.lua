809764e0c91cf3aca38bc83da0b0d1898e3246e1
koodihirmu <tomvar98@gmail.com>
2024-10-10 19:12:53 +0300

    stuff and shit

--- lua/custom/plugins/my_autocmds.lua
+++ lua/custom/plugins/my_autocmds.lua
@@ -1,26 +1,16 @@
--- local bufnr = 40
-
-local git_root = 'git rev-parse --show-toplevel'
+local bufnr = 19
 
 vim.api.nvim_create_autocmd('bufEnter', {
   group = vim.api.nvim_create_augroup('Mygroup', { clear = true }),
-  -- pattern = '*.lua',
+  pattern = '*.lua',
   callback = function()
-    -- vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, {}) -- clear lines
-    vim.fn.jobstart(git_root, {
+    vim.fn.jobstart({ 'git', 'root' }, {
       stdout_buffered = true,
       on_stdout = function(_, data)
         if data then
-          local git_dir = data[1]
-          if git_dir ~= "" then
-            print(git_dir)
-          else
-            print("git root directory not found")
-          end
+          vim.api.nvim_buf_set_lines(bufnr, -1, -1, false, { data })
         end
       end,
     })
   end,
 })
-
---  NOTE: git config --global alias.root 'rev-parse --show-toplevel'
