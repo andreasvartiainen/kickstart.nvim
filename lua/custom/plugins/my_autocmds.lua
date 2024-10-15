4452aa412caf097bb3c3d5e4c309e2edd68bd888
koodihirmu <tomvar98@gmail.com>
2024-10-15 19:03:54 +0300

    autocmd stuff

--- lua/custom/plugins/my_autocmds.lua
+++ lua/custom/plugins/my_autocmds.lua
@@ -1,16 +1,11 @@
-local bufnr = 19
-
-vim.api.nvim_create_autocmd('bufEnter', {
-  group = vim.api.nvim_create_augroup('Mygroup', { clear = true }),
-  pattern = '*.lua',
+vim.api.nvim_create_autocmd('VimEnter', {
+  desc = 'Auto select virtualenv Nvim open',
+  pattern = '*',
   callback = function()
-    vim.fn.jobstart({ 'git', 'root' }, {
-      stdout_buffered = true,
-      on_stdout = function(_, data)
-        if data then
-          vim.api.nvim_buf_set_lines(bufnr, -1, -1, false, { data })
-        end
-      end,
-    })
+    local venv = vim.fn.findfile('pyproject.toml', vim.fn.getcwd() .. ';')
+    if venv ~= '' then
+      require('venv-selector').retrieve_from_cache()
+    end
   end,
+  once = true,
 })
