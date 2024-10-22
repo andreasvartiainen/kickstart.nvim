0285827ad118f5b533804c29f4afc04b52149652
koodihirmu <tomvar98@gmail.com>
2024-10-22 22:20:46 +0300

    zathura working vimtex working

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -23,6 +23,7 @@ require('lazy').setup({
   require 'kickstart.plugins.cmake_tools',
   require 'kickstart.plugins.heirline', -- zeioth heirline components
   require 'kickstart.plugins.presence', -- plugin for discord rich presence
+  require 'kickstart.plugins.vimtex-config',
   { 'kylechui/nvim-surround', version = '*', event = 'VeryLazy', config = true },
   { 'Bilal2453/luvit-meta', lazy = true },
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } }, -- Highlight todo, notes, etc in comments
