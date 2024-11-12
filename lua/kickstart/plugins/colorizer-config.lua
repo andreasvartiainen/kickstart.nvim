482625baecfe1ed42a471f6411df3862d9dcaf12
koodihirmu <tomvar98@gmail.com>
2024-11-12 21:02:49 +0200

    comments to colorizer-config.lua

--- lua/kickstart/plugins/colorizer-config.lua
+++ lua/kickstart/plugins/colorizer-config.lua
@@ -6,12 +6,12 @@ return {
       require('colorizer').setup({
         '*',
       }, {
-        RGB = true, -- #RGB hex codes
-        RRGGBB = true, -- #RRGGBB hex codes
+        RGB = true, -- #RGB hex codes #00f
+        RRGGBB = true, -- #RRGGBB hex codes #0000ff
         names = true, -- "Name" codes like Blue
-        RRGGBBAA = true, -- #RRGGBBAA hex codes
-        rgb_fn = true, -- CSS rgb() and rgba() functions
-        hsl_fn = true, -- CSS hsl() and hsla() functions
+        RRGGBBAA = true, -- #RRGGBBAA hex codes #0000ffff
+        rgb_fn = true, -- CSS rgb() and rgba() functions rgb(0,0,255)
+        hsl_fn = true, -- CSS hsl() and hsla() functions hsl(240, 100%, 50%)
         css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
         css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
         -- Available modes: foreground, background
