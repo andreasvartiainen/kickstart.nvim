24effbe96235999757636c3a5c7f4878857c145c
koodihirmu <tomvar98@gmail.com>
2024-11-12 11:48:08 +0200

    factoring

--- lua/kickstart/plugins/gitsigns.lua
+++ lua/kickstart/plugins/gitsigns.lua
@@ -1,5 +1,6 @@
+-- NOTE:
 -- Adds git related signs to the gutter, as well as utilities for managing changes
--- NOTE: gitsigns is already included in init.lua but contains only the base
+-- gitsigns is already included in init.lua but contains only the base
 -- config. This will add also the recommended keymaps.
 
 return {
--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -12,26 +12,23 @@ require('lazy').setup({
   require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
   require 'kickstart.plugins.which-key', -- shows keys to press after pressing a key
   require 'kickstart.plugins.fuzzy-finder',
-  require 'kickstart.plugins.venv-selector-config', -- plugin for selecting virtual environments for python
+  require 'kickstart.plugins.venv-selector-config', -- plugin for selecting python envs
   require 'kickstart.plugins.lsp-config', -- language server configuration
   require 'kickstart.plugins.autoformat', -- autoformatting
   require 'kickstart.plugins.vim-cmp-config', -- autocomplete
   require 'kickstart.plugins.theme-config', -- theme for the editor configured here
   require 'kickstart.plugins.mini-config', -- library of lua modules to improve Neovim
   require 'kickstart.plugins.treesitter-config', -- Parser generator tool for updating syntax as the source is edited
-  require 'kickstart.plugins.lazygit',
+  require 'kickstart.plugins.lazygit', -- remember to install lazygit for your system
   require 'kickstart.plugins.cmake_tools',
   require 'kickstart.plugins.heirline', -- zeioth heirline components
   require 'kickstart.plugins.presence', -- plugin for discord rich presence
-  require 'kickstart.plugins.vimtex-config',
+  require 'kickstart.plugins.vimtex-config', -- latex plugin
   { 'kylechui/nvim-surround', version = '*', event = 'VeryLazy', config = true },
   { 'Bilal2453/luvit-meta', lazy = true },
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } }, -- Highlight todo, notes, etc in comments
 
-  -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
-  --    This is the easiest way to modularize your config.
-  --
-  --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
+  -- NOTE:
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
   -- { import = 'custom.plugins' },
 }, {
