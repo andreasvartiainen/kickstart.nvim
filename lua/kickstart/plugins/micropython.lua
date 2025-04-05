fb8fa9081d66522b419418f663bc6cf3c0849a5a
Tomi <59865056+koodihirmu@users.noreply.github.com>
2025-04-05 10:54:52 +0300

    add keymapping for micropython

--- lua/kickstart/plugins/micropython.lua
+++ lua/kickstart/plugins/micropython.lua
@@ -1,3 +1,6 @@
 return {
   'jim-at-jibba/micropython.nvim',
+  config = function()
+    vim.keymap.set('n', '<leader>mr', require('micropython_nvim').run, { desc = 'Run the current buffer on micro controller' })
+  end,
 }
